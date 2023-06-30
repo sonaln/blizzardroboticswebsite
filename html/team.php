cfa5d0e5c1c12670b9b99d1803119a94996fc820
Sonal Naik <sonal.sachin.naik@gmail.com>
2023-06-30 01:00:45 -0700

    Update team.php

--- html/team.php
+++ html/team.php
@@ -17,7 +17,6 @@ require '../templates/header.php';
     <div class="row">
 
         <div class="responsive">
-
             <div class="col-lg-4 col-md-6">
                 <div class="member">
                     <div class="pic"><img alt="Blizzard Robotics" src="images/Screen%20Shot%202020-01-15%20at%208.12.44%20PM.png"
@@ -36,7 +35,6 @@ require '../templates/header.php';
         </div>
 
         <div class="responsive">
-
             <div class="col-lg-4 col-md-6">
                 <div class="member">
                     <div class="pic"><img alt="Blizzard Robotics" src="images/Screen%20Shot%202020-01-15%20at%208.12.44%20PM.png"
@@ -54,77 +52,6 @@ require '../templates/header.php';
             </div>
         </div>
 
-        <div class="responsive">
-            <div class="col-lg-4 col-md-6">
-                <div class="member">
-                    <div class="pic"><img alt="Blizzard Robotics" src="images/beter.png" width=250 height=300 class="img-fluid" alt=""></div>
-                    <div style="font-family: 'Bai Jamjuree';" class="member-info">
-                        <h4>Peter Xu</h4>
-                        <span>Assistant Co-Lead and CAD Lead</span>
-                        <div class="social">
-                            <a href=""><i class="icofont-facebook"></i></a>
-                            <a href=""><i class="icofont-instagram"></i></a>
-                            <a href=""><i class="icofont-linkedin"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="responsive">
-            <div class="col-lg-4 col-md-6">
-                <div class="member">
-                    <div class="pic"><img alt="Blizzard Robotics" src="images/abeer.jpg" width=250 height=300 class="img-fluid" alt=""></div>
-                    <div style="font-family: 'Bai Jamjuree';" class="member-info">
-                        <h4>Abeer Bajpai</h4>
-                        <span>Mechanical, Software, and Website Developer</span>
-                        <div class="social">
-                            <a href=""><i class="icofont-facebook"></i></a>
-                            <a href=""><i class="icofont-instagram"></i></a>
-                            <a href=""><i class="icofont-linkedin"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="column"></div>
-        <div class="responsive">
-            <div class="col-lg-4 col-md-6">
-                <div class="member"
-                ="fade-up" -delay="300">
-                <div class="pic"><img alt="Blizzard Robotics" src="images/aaron.jpg" width=250 height=300 class="img-fluid" alt=""></div>
-                <div style="font-family: 'Bai Jamjuree';" class="member-info">
-                    <h4>Aaron Li</h4>
-                    <span>Software Lead, Mechanical, and Website Developer</span>
-                    <div class="social">
-                        <a href=""><i class="icofont-facebook"></i></a>
-                        <a href=""><i class="icofont-instagram"></i></a>
-                        <a href=""><i class="icofont-linkedin"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="responsive">
-        <div class="col-lg-4 col-md-6">
-            <div class="member"
-            ="fade-up" -delay="300">
-
-            <div class="pic"><img alt="Blizzard Robotics" src="images/everest.png" width=200 height=300 class="img-fluid" alt=""></div>
-            <div style="font-family: 'Bai Jamjuree';" class="member-info">
-
-                <h4>Everest</h4>
-                <span>Team Mascot</span>
-                <div class="social">
-                    <a href=""><i class="icofont-facebook"></i></a>
-                    <a href=""><i class="icofont-instagram"></i></a>
-                    <a href=""><i class="icofont-linkedin"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
 </section>
 
 <?php
